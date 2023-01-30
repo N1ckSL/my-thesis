7bfc604700369aacd10c7b2b2989c4276f5cc4b3
N1ckSL <savilencu.n@gmail.com>
2023-01-30 22:30:34 +0200

    add loader

---
+++ src/components/UI/Loader.tsx
@@ -0,0 +1,11 @@
+import { LoaderSVG } from "../../static/icons";
+
+function Loader() {
+  return (
+    <div className="container mx-auto flex items-center justify-center">
+      <LoaderSVG />
+    </div>
+  );
+}
+
+export { Loader };
